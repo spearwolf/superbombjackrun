bad4139cf0f03649558a45dbcdbcab3707bec56c
Wolfger Schramm <wolfger@spearwolf.de>
2014-02-02 16:00:21 +0100

    jade integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        clean: ["public/js/app.min.js", ".tmp"],
+        clean: ["public/js/app.min.js", "public/css/app.css", "public/index.html", ".tmp"],
 
         browserify: {
             dist: {
@@ -40,9 +43,40 @@ module.exports = function(grunt) {
                     "public/css/app.css": "app/styles/app.less"
                 }
             }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    hostname: '*',
+                    port: 8000,
+                    base: 'public'
+                }
+            }
+        },
+
+        watch: {
+            files: ['app/**/*'],
+            tasks: ['build']
+        },
+
+        jade: {
+            compile: {
+                options: {
+                    data: function(dest, src) {
+                        return require('package.json');
+                    }
+                },
+                files: {
+                    "public/index.html": ["app/views/index.jade"]
+                }
+            }
         }
+
     });
 
-    grunt.registerTask('build', ['browserify', 'uglify', 'less']);
-    grunt.registerTask('default', 'build');
+    grunt.registerTask('build', ['browserify', 'uglify', 'less', 'jade']);
+    grunt.registerTask('serve', ['connect', 'watch']);
+
+    grunt.registerTask('default', 'serve');
 };
---
+++ README.md
@@ -0,0 +1,1 @@
+template for a canvas based javascript app. uses grunt, browserify, uglify, less and jade.
